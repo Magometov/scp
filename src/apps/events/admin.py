6d2120d30e65f6e42ca75071a6f97ead37884d0d
Анзор Магометов <magometov_a.z@mail.ru>
2024-05-08 14:43:22 +0300

    fix: fix remarks

--- src/apps/events/admin.py
+++ src/apps/events/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 from src.apps.events.models import Event
 from src.apps.invitations.models import Invitation
+from src.apps.invitations.services.handle_invitation import handle_invitation_check
 
 
-class InvitationInline(admin.TabularInline[Invitation, Invitation]):
+class InvitationInline(admin.TabularInline[Invitation, Event]):
     model = Invitation
     extra = 1
     readonly_fields = ("status",)
@@ -23,3 +24,11 @@ class EventAdmin(admin.ModelAdmin[Event]):
         "modified",
     )
     list_filter = ("created", "modified", "author", "start", "end")
+
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
+        if not instances:
+            super().save_formset(request, form, formset, change)
+        for instance in instances:
+            if handle_invitation_check(self, request, instance.event, instance.attendee):
+                super().save_formset(request, form, formset, change)
--- src/apps/invitations/admin.py
+++ src/apps/invitations/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from src.apps.invitations.services.handle_invitation import handle_invitation_check
+
 from .models import Invitation
 
 
@@ -13,3 +15,7 @@ class InvitationAdmin(admin.ModelAdmin[Invitation]):
         "modified",
     )
     list_filter = ("created", "modified", "attendee", "event")
+
+    def save_model(self, request, obj, form, change):
+        if handle_invitation_check(self, request, obj.event, obj.attendee):
+            super().save_model(request, obj, form, change)
