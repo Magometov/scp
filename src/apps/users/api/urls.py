f609e356eb2cb245ae2023c1215e42c47b76f1ce
Anzor <magometov_a.z@mail.ru>
2024-05-19 13:04:08 +0300

    fix: fexed architecture in users app, deleted api dir

--- src/apps/users/api/urls.py
+++ src/apps/users/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from src.apps.api.views.user_views import UserCreateAPIView
+from src.apps.users.api.views import UserCreateAPIView
 
 urlpatterns = [
     path("", UserCreateAPIView.as_view()),
