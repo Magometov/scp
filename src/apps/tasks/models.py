ed5757974d5741d4cfcae4c9a16fe1c8d44a2c29
Анзор Магометов <magometov_a.z@mail.ru>
2024-05-21 16:31:24 +0300

    feat(base:softdelete): added softdelete logic for Task model

--- src/apps/tasks/models.py
+++ src/apps/tasks/models.py
@@ -6,26 +6,16 @@ from django.utils.translation import gettext_lazy as _
 from django_stubs_ext.db.models import TypedModelMeta
 
 from src.apps.base.models import BaseModel, TimeStampedModel
+from src.apps.base.softdelete.models import SoftDeleteModel
 
 if TYPE_CHECKING:
     from datetime import datetime
     from src.apps.users.models import User
-    from typing import Any
 
 from .const import TaskPriority, TaskStatus
 
 
-class MyQuerySet(models.query.QuerySet):
-    def delete(self):
-        self.update(status=TaskStatus.cancelled)
-
-
-class NoDeleteManager(models.Manager):
-    def get_queryset(self):
-        return MyQuerySet(self.model, using=self._db)
-
-
-class Task(BaseModel, TimeStampedModel):
+class Task(SoftDeleteModel, BaseModel, TimeStampedModel):
     title: "models.CharField[str, str]" = models.CharField(verbose_name=_("Title"), max_length=50)
     description: "models.TextField[str, str]" = models.TextField(verbose_name=_("Description"), blank=True)
     status: "models.PositiveSmallIntegerField[TaskStatus, TaskStatus]" = models.PositiveSmallIntegerField(
@@ -49,8 +39,6 @@ class Task(BaseModel, TimeStampedModel):
         )
     )
 
-    objects = NoDeleteManager()
-
     class Meta(TypedModelMeta):
         verbose_name = _("Task")
         verbose_name_plural = _("Tasks")
@@ -65,10 +53,6 @@ class Task(BaseModel, TimeStampedModel):
                 name="%(app_label)s_%(class)s_end_start_both_or_none",
             ),
         ]
-
-    def delete(self, *args: "Any", **kwargs: "Any") -> None:  # type: ignore
-        self.status = TaskStatus.cancelled
-        self.save()
 
     def __str__(self) -> str:
         return f"Task from {self.author}"
