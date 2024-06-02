2b683d93e633ddb0552089143aae1389d484686b
Anzor <magometov_a.z@mail.ru>
2024-06-02 15:50:02 +0300

    fix: fixed remarks

--- src/apps/users/tests/test_users.py
+++ src/apps/users/tests/test_users.py
@@ -1,25 +1,27 @@
 import re
 from datetime import timedelta
-from typing import Any
+from typing import TYPE_CHECKING, cast
 from urllib.parse import urlencode, urljoin
 
 import pytest
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
-from django.utils import timezone
+from django.utils.safestring import SafeString
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
+from rest_framework_simplejwt.settings import api_settings as jwt_settings
 
 from src.apps.api_auth.services.tokens import create_access_token
 from src.apps.users.models import User
 from src.conftest import UserData
 
+if TYPE_CHECKING:
+    from freezegun.api import FrozenDateTimeFactory
 
-def get_link_from_message(message: str) -> Any:
-    pattern = rf"{settings.APP_SITE}{reverse("users:verify-user")}\?token=[\w\.-]+"
-    matches = re.findall(pattern, message)
-    return matches[0] if matches else None
+JWT_PATTERN = r"[\w\.-]+"
+USER_VERIFY_ENDPOINT = reverse("users:verify-user")
+VERIFICATION_URL_PATTERN = rf"{settings.APP_SITE}{USER_VERIFY_ENDPOINT}\?token={JWT_PATTERN}"
 
 
 @pytest.mark.django_db()
@@ -39,8 +41,9 @@ def test_user_email_verification(
     message = mailoutbox[0]
     assert message.to == [user.email]
 
-    href = get_link_from_message(str(message.body))
-    response_from_verify = client.get(href)
+    text = cast(SafeString, message.body)
+    assert (match := re.search(VERIFICATION_URL_PATTERN, text))
+    response_from_verify = client.get(match.group(0))
     assert response_from_verify.status_code == status.HTTP_200_OK
 
     user = User.objects.get(pk=response.data["id"])
@@ -52,13 +55,13 @@ def test_expired_token(
     api_client: type[APIClient],
     user_create: User,
     mailoutbox: list[EmailMultiAlternatives],
-    freezer: Any,
+    freezer: "FrozenDateTimeFactory",
 ) -> None:
     user = user_create
     client = api_client()
-    initial_datetime = timezone.now()
+
     token = create_access_token(user)
-    freezer.move_to(initial_datetime + timedelta(days=2))
+    freezer.move_to(jwt_settings.REFRESH_TOKEN_LIFETIME + timedelta(hours=1))
     endpoint = reverse("users:verify-user")
     verify_email_link = urljoin(settings.APP_SITE, endpoint)
     params = {"token": token}
